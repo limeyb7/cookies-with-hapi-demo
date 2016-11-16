8b637e264c50e18a96fd684c6bb090480b681791
msmichellegar <msmichellegar@gmail.com>
2016-11-16 23:00:28 +0000

    updates boilerplate for es6, refactors server and stops server in tests

--- server.js
+++ server.js
@@ -1,19 +1,19 @@
-var Hapi = require('hapi');
-var Inert = require('inert');
-var routes = require('./lib/routes.js');
+const Hapi = require('hapi');
+const Inert = require('inert');
+const routes = require('./lib/routes.js');
 
-var server = new Hapi.Server();
-
+const server = new Hapi.Server();
 
 server.register(Inert, function () {
     server.connection({
         port: process.env.PORT || 8080
     });
-    server.route(routes);
-});
 
-server.start(function() {
-    console.log('Server running at: ' + server.info.uri + '!');
+    server.route(routes);
+
+    server.start(function() {
+        console.log('Server running at: ' + server.info.uri + '!');
+    });
 });
 
 module.exports = server;
--- test/test.js
+++ test/test.js
@@ -1,9 +1,10 @@
-var test = require("tape");
-var server = require('../server.js');
+const test = require("tape");
+const server = require('../server.js');
 
 test("'/' returns 200 statusCode", function (t) {
     server.inject({method: 'GET', url: '/'}, function (res) {
         t.equal(res.statusCode, 200, 'page loaded successfully');
+        server.stop();
         t.end();
     });
 });
@@ -11,6 +12,7 @@ test("'/' returns 200 statusCode", function (t) {
 test("'/static/{file*}' returns 200 statusCode", function (t) {
     server.inject({method: 'GET', url: '/static/public/css/main.css'}, function (res) {
         t.equal(res.statusCode, 200, 'static file retrieved');
+        server.stop();
         t.end();
     });
 });
